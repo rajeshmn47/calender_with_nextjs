a108c91d382351f4db48536922768fa4688d376f
rajeshmn47 <rajeshmn47@gmail.com>
2022-06-21 06:17:21 +0530

    make it better

--- pages/components/calendar-days.js
+++ pages/components/calendar-days.js
@@ -7,51 +7,52 @@ import {useState,useEffect} from 'react';
 
 export default function CalendarDays(props) {
   const [value, setValue] =useState(props.day);
-  const [calenderdays, setCalenderdays] =useState(props.day);
+  const [calenderdays, setCalenderdays] =useState();
   const [timings, setTimings] =useState(props.day);
   const[task,setTask]=useState()
 
 const time=new Array(24).fill(null)
   const firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
   const weekdayOfFirstDay = firstDayOfMonth.getDay();
+
+useEffect(()=>{
   var currentDays = [];
-function bcg(){
- 
-
-  for (let day = 0; day < 42; day++) {
-    if (day === 0 && weekdayOfFirstDay === 0) {
-      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
-    } else if (day === 0) {
-      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
-    } else {
-      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+  function bcg(){
+   
+  
+    for (let day = 0; day < 42; day++) {
+      if (day === 0 && weekdayOfFirstDay === 0) {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+      } else if (day === 0) {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
+      } else {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+      }
+  
+      let calendarDay = {
+        currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
+        date: (new Date(firstDayOfMonth)),
+        month: firstDayOfMonth.getMonth(),
+        number: firstDayOfMonth.getDate(),
+        selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
+        year: firstDayOfMonth.getFullYear(),
+        task:null,
+        time:null
+      }
+  
+      currentDays.push(calendarDay);
     }
-
-    let calendarDay = {
-      currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
-      date: (new Date(firstDayOfMonth)),
-      month: firstDayOfMonth.getMonth(),
-      number: firstDayOfMonth.getDate(),
-      selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
-      year: firstDayOfMonth.getFullYear(),
-      task:null,
-      time:null
-    }
-
-    currentDays.push(calendarDay);
   }
-}
-
+  bcg()
+  setCalenderdays(currentDays)
+},[])
   const handleChange = (newValue) => {
     console.log(newValue.getDate(),newValue.getDay(),'rajesh')
     setValue(newValue)
     const day={year:newValue.getFullYear(),month:newValue.getMonth(),number:newValue.getDate()}
     props.changeCurrentDay(day)
   };
-useEffect(()=>{
-  bcg()
-  setCalenderdays(currentDays)
-},[])
+
   const  handlesubmit=(e)=>{
 e.preventDefault()
 console.log('raesh')
@@ -67,7 +68,7 @@ garuda.forEach(element,index => {
     <>
     <div className="table-content">
       {
-        currentDays.map((day) => {
+        calenderdays?.map((day) => {
           return (
             <div className={"calendar-day" + (day.currentMonth ? " current" : "") + (day.selected ? " selected" : "")}
                   onClick={() => props.changeCurrentDay(day)}>
